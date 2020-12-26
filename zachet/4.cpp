883110b6cd476a1a3be362dc574ad4d03c50f31b
Nikita Dolbak <nikitf28@yandex.ru>
2020-12-26 11:10:29 +0800

    task4

---
+++ zachet/4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+long long countNum(long long num){
+    long long i = 0;
+    while (num > 0){
+        num/=10;
+        i++;
+    }
+    return i;
+}
+
+long long getNthNum(long long num, long long nth){
+    //int length = countNum(num);
+    for (long long i = 0; i < nth; i++){
+        num /= 10;
+    }
+    return num % 10;
+}
+
+int main(){
+    long long k;
+    cin >>k;
+
+    long long sum = 0;
+    long long i = 0;
+    long long prev = 0;
+
+    while (sum < k){
+        i++;
+        prev = sum;
+        sum += countNum(i*i);
+    }
+
+    //cout <<i*i <<" " <<sum <<endl;
+
+    cout <<getNthNum(i*i, sum - k);
+}
