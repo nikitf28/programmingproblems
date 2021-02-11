121833d7c9edf23fa1500541786db6f77bd6f9fa
Nikita Dolbak <nikitf28@yandex.ru>
2021-02-11 16:42:40 +0800

    e task

---
+++ e/withE.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+bool ifEasy(long long num){
+    for (int i = 2; i < sqrt(num); i++){
+        if (num % i == 0){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    ifstream fin("e2mil.txt");
+    string e, buffer;
+
+    while (fin >> buffer){
+        e += buffer;
+    }
+
+    for (int i = 9; i < e.length(); i++){
+        string strNum = "";
+        for (int j = i - 9; j <= i; j++){
+            strNum += e[j];
+        }
+        cout <<i-8 <<". " <<strNum <<endl;
+        long long num = stoll(strNum);
+        if (!ifEasy(num)){
+            cout <<"================" <<endl <<endl <<num;
+            return 0;
+        }
+    }
+
+}
