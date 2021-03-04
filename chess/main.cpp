239ca4cbf3d60fea260db95f69348a4673d7b494
Nikita Dolbak <nikitf28@yandex.ru>
2021-03-05 00:49:56 +0800

    ferzi

--- chess/main.cpp
+++ chess/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<cstdlib>
 
 using namespace std;
 
@@ -18,9 +19,41 @@ chessNum letterToNum(string letters){
     if (let1 < 'A' || let1 > 'H' || let2 < '1' || let2 > '8'){
         return ans;
     }
-    chessNum.col = let1 - 'A';
-    chessNum.row = let2 - '1';
+    ans.col = let1 - 'A';
+    ans.row = let2 - '1';
     return ans;
+}
+
+char numToChar(int n){
+    return n + 'A';
+}
+
+void checkFerz(const int board[8][8], int row, int col){
+    for (int i = col - 1; i >= 0; i--){
+        if (board[row][i] == 1){
+            cout <<numToChar(col)<<row+1 <<" <---> " <<numToChar(i)  <<row+1 <<endl;
+            break;
+        }
+    }
+    for (int j = row - 1; j >= 0; j--){
+        if (board[j][col] == 1){
+            cout <<numToChar(col)<<row+1 <<" <---> " <<numToChar(col)  <<j+1 <<endl;
+            break;
+        }
+    }
+
+    for (int i = col - 1, j = row-1; i >= 0 && j >= 0; i--, j--){
+        if (board[j][i] == 1){
+            cout <<numToChar(col)<<row+1 <<" <---> " <<numToChar(i)  <<j+1 <<endl;
+            break;
+        }
+    }
+    for (int i = col + 1, j = row-1; i < 8, j >= 0; i++, j--){
+        if (board[j][i] == 1){
+            cout <<numToChar(col)<<row+1 <<" <---> " <<numToChar(i)  <<j+1 <<endl;
+            break;
+        }
+    }
 }
 
 int main(){
@@ -29,8 +62,27 @@ int main(){
     cout <<"Enter number of chess figures: ";
     cin >>num;
 
+    int board[8][8];
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            board[i][j] = 0;
+        }
+    }
+
     for (int i = 0; i < num; i++){
         string letters;
         cin >>letters;
+        chessNum ferz = letterToNum(letters);
+        board[ferz.row][ferz.col] = 1;
     }
+
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            if (board[i][j] == 1){
+                //cout <<i <<" " <<j <<endl;
+                checkFerz(board, i, j);
+            }
+        }
+    }
+
 }
